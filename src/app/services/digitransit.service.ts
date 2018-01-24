16f73be89d47e417f8fad70f39cb69982b1710b6
Kseniia Chumachenko <timeofneon@gmail.com>
2018-01-24 15:20:28 +0200

    Part 1,2 from 24.01.18

---
+++ src/app/services/digitransit.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable()
+export class DigitransitService {
+
+  apiUrl = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
+
+  constructor(private http: HttpClient) { }
+
+  getRoutes(stopName) {
+    const body = `{
+                      stops(name: "${stopName}") {
+                        name
+                        patterns {
+                          name
+                          route {
+                            shortName
+                            longName
+                          }
+                          directionId
+                        }
+                      }
+                    }`;
+    const settings = {
+      headers: new HttpHeaders().set('Content-Type', 'application/graphql')
+    };
+    return this.http.post(this.apiUrl, body, settings);
+  }
+
+}
